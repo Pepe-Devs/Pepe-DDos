8ec24a897add356d7bd565bbef3f1b7790d98227
WoodMan <85753549+nordbearbotdev@users.noreply.github.com>
2022-05-17 19:47:28 +0300

    Create PepeDDos.py

---
+++ PepeDDos.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import sys
+import time
+import socket
+import struct
+import threading
+from random import randint
+from optparse import OptionParser
+from pinject import IP, UDP
